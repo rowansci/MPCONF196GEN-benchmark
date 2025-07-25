b90be3010d3cc6204ab23d883d8c26a65d1c931e
EliasLMann <eliaslmann@gmail.com>
2025-07-25 13:39:25 +0000

    computed SMILES mapping

---
+++ tools/SMILES_and_rotateable_bonds.py
@@ -0,0 +1,31 @@
+from rdkit import Chem
+from rdkit.Chem import rdMolDescriptors
+
+
+SMILES = {
+        "CAMVES_I": r"C[C@@H]1NC(=O)[C@H](C)NC(=O)[C@@H]2CCCN2C(=O)[C@H]2CCCN2C(=O)[C@H](C)NC(=O)[C@H](C)NC1=O",
+        "CHPSAR_I": r"CN1CC(=O)N(C)CC(=O)N(C)CC(=O)N(C)CC(=O)N(C)CC(=O)N(C)CC(=O)N(C)CC1=O",
+        "COHVAW_I": r"C=C1C(=O)O[C@@H]2C/C(COC(C)=O)=C\CC[C@](C)(O)C[C@@H](OC(=O)/C(=C\C)COC(C)=O)[C@@H]12",
+        "FGG55": r"N[C@H](Cc1ccccc1)C(=O)NCC(=O)NCC(=O)O",
+        "GFA01": r"C[C@@H](NC(=O)[C@H](Cc1ccccc1)NC(=O)CN)C(=O)O",
+        "GGF01": r"NCC(=O)NCC(=O)N[C@@H](Cc1ccccc1)C(=O)O",
+        "GS464992_I": r"COC(=O)[C@@H]1CCCN(C(=O)[C@H](Cc2cccc(O)c2)NC(=O)[C@@H](NC(C)=O)C(C)C)N1",
+        "GS557577_I": r"CO[C@@H]1CC/C=C/c2cccc(c2)COC(=O)[C@@H]2CCCN(N2)C(=O)[C@H](Cc2cccc(O)c2)NC(=O)[C@H](C(C)C)NC(=O)[C@@H]1C",
+        "POXTRD_I": r"O=C1COCC(=O)OCC(=O)NCCOCCN1",
+        "SANGLI_I": r"CC(=O)CC[C@H]1C(=O)N[C@@H](C(C)C)C(=O)N[C@@H](Cc2cccc(O)c2)C(=O)N2CCC[C@H](N2)C(=O)OCC/C=C/C=C/[C@H](O)[C@H](C)[C@H]1O",
+        "WG01": r"N[C@@H](Cc1c[nH]c2ccccc12)C(=O)NCC(=O)O",
+        "WGG01": r"N[C@@H](Cc1c[nH]c2ccccc12)C(=O)NCC(=O)NCC(=O)O",
+        "YIVNOG_I": r"C1CN2CC(NCCN(CC3C=CC=CC=3)CCNC(CN3CCN(CC(NCCN(CC4C=CC=CC=4)CCNC(CN(CC2)C1)=O)=O)CCC3)=O)=O",
+        }
+
+
+with open("SMILES.txt", "w") as f:
+    f.write("rotatable_bonds\tSMILES\tmolecule_id\n")
+    for name, smi in SMILES.items():
+        mol = Chem.MolFromSmiles(smi)
+        if mol is None:
+            print(f"Skipping {name}: invalid SMILES")
+            continue
+        smi_canon = Chem.MolToSmiles(mol)
+        n_rot = rdMolDescriptors.CalcNumRotatableBonds(mol)
+        f.write(f"{n_rot}\t{smi_canon}\t{name}\n")
