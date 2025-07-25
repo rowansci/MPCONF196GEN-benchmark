5927799af571f313fe98f5c50be95f7f436d0abf
Elias Mann <70070619+EliasLMann@users.noreply.github.com>
2025-07-25 16:43:35 -0400

    Update get_egret_energies.py

--- tools/get_egret_energies.py
+++ tools/get_egret_energies.py
@@ -1,15 +1,17 @@
+import os
+
 import ase.io
 import torch
 from ase.calculators.calculator import all_changes
 from mace.calculators import mace_off
-import os
 
 KCAL_PER_MOL = 23.0605
 MOLECULES_DIR = "molecules"
 
 calc = mace_off(model="tools/EGRET_1.model", default_dtype="float64")
 
-def process_subdir(subdir: str):
+
+def process_subdir(subdir: str) -> None:
     in_xyz  = os.path.join(subdir, "crest_conformers.xyz")
     out_xyz = os.path.join(subdir, "crest_conformers_egret.xyz")
 
@@ -21,7 +23,8 @@ def process_subdir(subdir: str):
     ase.io.write(out_xyz, all_confs, format="extxyz")
     print(f"wrote {out_xyz}")
 
-def main():
+
+def main() -> None:
     for sub in sorted(os.listdir(MOLECULES_DIR)):
         subpath = os.path.join(MOLECULES_DIR, sub)
         if os.path.isdir(subpath) and os.path.exists(os.path.join(subpath, "crest_conformers.xyz")):
@@ -29,6 +32,7 @@ def main():
             process_subdir(subpath)
         else:
             print(f"Skipping {sub!r}")
+
 
 if __name__ == "__main__":
     main()
